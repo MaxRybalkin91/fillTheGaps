00471af09fcbcfd139d947152976da84a67307d3
Maksim Rybalkin <maxrybalkin91@gmail.com>
2024-01-11 20:37:11 +0400

    Add thread pools

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -1,16 +1,32 @@
 package course.concurrency.m5_streams;
 
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadPoolTask {
 
-    // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        return new ThreadPoolExecutor(
+                1,
+                1,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LIFOBlockingQueue<>()
+        );
     }
 
-    // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8,
+                60, TimeUnit.SECONDS,
+                new SynchronousQueue<>(), new ThreadPoolExecutor.DiscardPolicy());
+    }
+
+    private static class LIFOBlockingQueue<E> extends LinkedBlockingDeque<E> {
+        @Override
+        public E take() throws InterruptedException {
+            return super.takeLast();
+        }
     }
 }
